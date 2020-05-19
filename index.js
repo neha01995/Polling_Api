1b4caf6ceaefe016cd8d505e2d13df5f1b0fa20e
Neha Singla <nehasingla7180@gmail.com>
2020-05-19 22:33:17 +0530

    server is running on port successfully

---
+++ index.js
@@ -0,0 +1,28 @@
+const express= require('express');
+const bodyParser = require('body-parser');
+const port = 8000;
+// create express app
+const app = express();
+
+// db config
+// const db = require("./config/mongoose");
+
+
+// parse requests of content type
+app.use(bodyParser.urlencoded({extended: true}));
+// parse request of content-type
+app.use(bodyParser.json());
+
+// app.use('/', require("./routes"));
+
+// Defining a simple route
+// app.get('/', (req, res) => {
+//     res.json({"message": "welcome guys" });
+// });
+
+
+// Listen for requests
+app.listen(port, function(err){
+    if(err){console.log(`Error in running the server, ${err}`)};
+    console.log(`Server is running on port: ${port}`);
+})
